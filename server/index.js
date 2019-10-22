a8739ae9bb161f6bb99e0cb2e920824148e48439
Mike Flinn <mflinn92@gmail.com>
2019-10-22 19:05:24 -0400

    Now renders all but the item details module in proxy. Page layout isnt quite right yet, photo carouseltakes up full page width

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const PORT = 4321;
 app = express();
 
 app.use(proxy('/api/checkout', {target: 'http://localhost:1234'}));
-app.use(proxy('/api/seller', {target: 'http://localhost:5000'}))
+app.use(proxy('/api/seller', {target: 'http://localhost:5000'}));
+app.use(proxy('/products', {target: 'http://localhost:3000'}));
 app.use(express.static('./public'));
 
 app.listen(PORT, () => {
