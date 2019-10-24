e38e6267004442d25293eda835d2dd7e4b2c3242
Mike Flinn <mflinn92@gmail.com>
2019-10-24 14:37:57 -0400

    Now renders updated version of each module, all modules can search for a product by id number

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ app = express();
 app.use(proxy('/api/checkout', {target: 'http://localhost:1234'}));
 app.use(proxy('/api/seller', {target: 'http://localhost:5000'}));
 app.use(proxy('/products', {target: 'http://localhost:3000'}));
-app.use(proxy('/index', {target: 'http://localhost:4000'}));
+app.use(proxy('/api', {target: 'http://localhost:4000'}));
 app.use(express.static('./public'));
 
 app.listen(PORT, () => {
