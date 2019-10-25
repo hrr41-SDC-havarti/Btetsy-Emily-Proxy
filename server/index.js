600ed52981e6a2438e7e8a6873f33b9af31c1715
Mike Flinn <mflinn92@gmail.com>
2019-10-24 20:34:47 -0400

    Now uses the checkout module that is deployed on aws rather than a locally run instance

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 const PORT = 4321;
 app = express();
 
-app.use(proxy('/api/checkout', {target: 'http://localhost:1234'}));
+app.use(proxy('/api/checkout', {target: 'http://ec2-18-223-235-255.us-east-2.compute.amazonaws.com:1234/'}));
 app.use(proxy('/api/seller', {target: 'http://localhost:5000'}));
 app.use(proxy('/products', {target: 'http://localhost:3000'}));
 app.use(proxy('/api', {target: 'http://localhost:4000'}));
