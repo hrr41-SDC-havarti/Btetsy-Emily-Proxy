20133bd4321fe53e6249714dd15b46e97a8e2795
Mike Flinn <mflinn92@gmail.com>
2019-10-25 17:30:51 -0400

    Added logo to top of page. updtated to use aws checkout module

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ app = express();
 
 app.use(proxy('/api/checkout', {target: 'http://ec2-18-223-235-255.us-east-2.compute.amazonaws.com:1234/'}));
 app.use(proxy('/api/seller', {target: 'http://localhost:5000'}));
-app.use(proxy('/products', {target: 'http://localhost:3000'}));
+app.use(proxy('/products', {target: 'http://localhost:3333'}));
 app.use(proxy('/api', {target: 'http://localhost:4000'}));
 app.use(express.static('./public'));
 
